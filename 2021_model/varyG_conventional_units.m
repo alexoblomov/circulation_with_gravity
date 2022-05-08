24198e36bf90683b42ac3f5ac8872dd2f38508b9
zanahmad <60706569+zanahmad@users.noreply.github.com>
2022-05-08 12:03:00 -0400

    made plots with familiar units for presentation

---
+++ 2021_model/varyG_conventional_units.m
@@ -0,0 +1,174 @@
+clear all
+close all
+
+Psa_u_star = 100*1333; %mmHg * [ dynes/(mmHg*cm^2) ]
+Psa_u = Psa_u_star; 
+dP_RA = 2*1333;
+
+height = 172; %cm
+Hu = (1/(3*2))*height;%upper  
+Hl = -(2/(3*2))*height;%lower
+
+rho = 1; %g/cm^3
+g_earth = 980; %gravitational acceleration cm/s^2
+G = linspace(980,980*10, 10000); %cm/s^2
+
+Rs = (17.86)*1333/(1000/60); %systemic resistance (mmHg/(liters/s))
+Gs = 1/Rs;
+Gs_u = (1/3)*Gs;
+Gs_l = (2/3)*Gs; 
+Rs_l = 1/Gs_l;
+Rs_u = 1/Gs_u; 
+Rp = (1.61*1333)/(1000/60); %pulmonic resistance (mmHg/(liters/s))
+
+C_RVD = (0.0350/1333)*1000; %right-ventricular diastolic compliance (liters/mmHg)
+C_LVD = (0.00583/1333)*1000; %left-ventricular diastolic compliance (liters/mmHg)
+
+Csa_l = (2/3)*(0.00175/1333)*1000;
+Csa_u = (1/3)*(0.00175/1333)*1000; 
+Csv_l = (2/3)*(0.09/1333)*1000;
+Csv_u = (1/3)*(0.09/1333)*1000;
+Cs_l = Csa_l + Csv_l;
+
+Cpa = (0.00412/1333)*1000; 
+Cpv = (0.01/1333)*1000; 
+Cp = Cpa + Cpv;
+Vtotal = 5.0*1000; %cm^3
+
+Csa = Csa_l+Csa_u;
+Gs = 1/Rs_u + 1/Rs_l; 
+Gs_l = 1/Rs_l;
+Ts = Csa_u*Rs_u; %Csa_l*Rs_l
+Tp = Rp*Cpa;
+Csa = Csa_u+Csa_l;
+
+P_thorax = linspace(-4*1333,3*1333, 8); %mmHg * dynes/(mmHg*cm^2)
+%P_thorax = -4*1333;
+P_RA = P_thorax+dP_RA;
+
+Vd_total_vec = zeros(1,length(G)); 
+sol_Vd_Pthorax_G = zeros(length(P_thorax), length(G)); 
+ sol_Q_Pthorax_G = zeros(length(P_thorax), length(G)); 
+ sol_F_Pthorax_G = zeros(length(P_thorax), length(G)); 
+ sol_Ppa_Pthorax_G = zeros(length(P_thorax),length(G));
+ 
+for j = 1:length(P_thorax)
+    for i = 1:length(G)
+        if P_thorax(j) <= -dP_RA %CASE 1
+            Vd_total = Vtotal - Cp*(C_RVD/C_LVD)*(dP_RA) - ...
+            (Tp*Gs+Csa)*Psa_u_star - (Tp*Gs_l+Csa_l)*rho*G(i)*Hu...
+            - Cs_l*rho*G(i)*(-Hl);
+            Q = ((1/Rs_u)+(1/Rs_l))*Psa_u_star+ rho*G(i)*Hu/Rs_l;
+            F = Q/(C_RVD*(P_RA(j)-P_thorax(j)));
+            Ppv = P_thorax(j) + (C_LVD/C_RVD)*dP_RA;
+            Ppa = Ppv+Q*Rp;
+            
+        elseif P_thorax(j) > -dP_RA && P_thorax(j) < rho*G(i)*Hu - dP_RA %CASE 2
+            Vd_total = Vtotal - Cp*(C_RVD/C_LVD)*dP_RA - ...
+            (Tp*Gs+Csa)*Psa_u_star - (Tp*Gs_l+Csa_l)*rho*G(i)*Hu ...
+            - Cs_l*rho*G(i)*(-Hl) - (Csv_l-Tp*Gs_l)*(P_thorax(j)+dP_RA);
+            
+            Psv_l = -rho*G(i)*Hl + P_thorax(j) + dP_RA;
+            Psv_u = 0; 
+            Psa_l = Psa_u_star + rho*G(i)*(Hu-Hl); 
+
+            Qs_u = Psa_u_star/Rs_u;
+            Qs_l = (Psa_l-Psv_l)/Rs_l;
+            Q = Qs_u + Qs_l;
+            F = Q/(C_RVD*(dP_RA));
+            Ppv = P_thorax(j) + (C_LVD/C_RVD)*dP_RA;
+            Ppa = Ppv+Q*Rp;
+            
+        elseif P_thorax(j) >= rho*G(i)*Hu - dP_RA %CASE 3
+            Vd_total = Vtotal - Cp*(C_RVD/C_LVD)*dP_RA ...
+            - (Tp*Gs+Csa)*Psa_u_star - (Tp*Gs+Csa_l-Csv_u)*rho*G(i)*Hu...
+            - Cs_l*rho*G(i)*(-Hl) - (Csv_l-Tp*Gs)*(P_thorax(j)+dP_RA);
+        
+            Psv_l = P_thorax(j) + dP_RA + rho*G(i)*(-Hl);
+            Psv_u = P_thorax(j) + dP_RA - rho*G(i)*Hu ; 
+            Psa_l = Psa_u_star + rho*G(i)*(Hu-Hl); 
+
+            Qs_u = (Psa_u_star - Psv_u)/Rs_u;
+            Qs_l = (Psa_l-Psv_l)/Rs_l;
+            Q = Qs_u + Qs_l;
+            F = Q/(C_RVD*(dP_RA));
+            Ppv = P_thorax(j) + (C_LVD/C_RVD)*dP_RA;
+            Ppa = Ppv+Q*Rp;
+        end
+        if Vd_total > 0 
+        Vd_total_vec(i) = Vd_total; 
+      
+          Q_vec(i) = Q; 
+          F_vec(i) = F; 
+          Ppa_vec(i) = Ppa; 
+          
+        else
+          Vd_total_vec(i) = NaN; 
+      
+          Q_vec(i) = NaN; 
+          F_vec(i) = NaN; 
+          Ppa_vec(i) = NaN;
+        end
+    end
+        sol_Vd_Pthorax_G(j,:) = Vd_total_vec; 
+        sol_Q_Pthorax_G(j,:) = Q_vec; 
+        sol_F_Pthorax_G(j,:) = F_vec; 
+        sol_Ppa_Pthorax_G(j,:) = Ppa_vec;
+end
+
+%conversions:
+G = G/100; 
+sol_Q_Pthorax_G = sol_Q_Pthorax_G*60/1000; 
+sol_F_Pthorax_G = sol_F_Pthorax_G*60;
+sol_Ppa_Pthorax_G = sol_Ppa_Pthorax_G/1333;
+sol_Vd_Pthorax_G = sol_Vd_Pthorax_G/1000;
+
+
+%family of plots for Reserve Volume vs. G for different Pthorax values: 
+% 
+figure(100)
+for i = 1:length(P_thorax)
+
+plot(G,sol_Vd_Pthorax_G(i,:))
+xlabel('G, m/s$^2$', 'interpreter', 'latex')
+ylabel('V$^0$, (L)', 'interpreter', 'latex')
+hold on
+end
+% xticks([1*9.80 2*9.80 3*9.80 4*9.80 5*9.80 6*9.80 7*9.80 8*9.80 9*9.80 10*9.80])
+% xticklabels({'g','2g','3g','4g','5g','6g','7g', '8g', '9g', '10g'})
+
+figure(101)
+for i = 1: length(P_thorax)
+
+plot(G,sol_Q_Pthorax_G(i,:))
+xlabel('G, m/s$^2$', 'interpreter', 'latex')
+ylabel('Q, (L/min)', 'interpreter', 'latex')
+hold on
+end
+% xticks([1*9.80 2*9.80 3*9.80 4*9.80 5*9.80 6*9.80 7*9.80 8*9.80 9*9.80 10*9.80])
+% xticklabels({'g','2g','3g','4g','5g','6g','7g', '8g', '9g', '10g'})
+
+
+figure(102)
+for i = 1:length(P_thorax)
+
+plot(G,sol_F_Pthorax_G(i,:))
+xlabel('G, m/s$^2$', 'interpreter', 'latex')
+ylabel('F, (min$^{-1}$)', 'interpreter', 'latex')
+hold on
+end
+% xticks([1*9.80 2*9.80 3*9.80 4*9.80 5*9.80 6*9.80 7*9.80 8*9.80 9*9.80 10*9.80])
+% xticklabels({'g','2g','3g','4g','5g','6g','7g', '8g', '9g', '10g'})
+
+
+
+figure(103)
+for i = 1:length(P_thorax)
+
+plot(G,sol_Ppa_Pthorax_G(i,:))
+xlabel('G, m/s$^2$', 'interpreter', 'latex')
+ylabel('Ppa, mmHg', 'interpreter', 'latex')
+hold on
+end
+% xticks([1*9.80 2*9.80 3*9.80 4*9.80 5*9.80 6*9.80 7*9.80 8*9.80 9*9.80 10*9.80])
+% xticklabels({'g','2g','3g','4g','5g','6g','7g', '8g', '9g', '10g'})
