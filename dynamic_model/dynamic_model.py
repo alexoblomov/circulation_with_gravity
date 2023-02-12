dbc6eac7235dfbf79fd8466bca4ac1260a5124e2
alexoblomov <alanna.kennard@gmail.com>
2023-02-12 19:11:52 +0100

    linted dynamic model

--- dynamic_model/dynamic_model.py
+++ dynamic_model/dynamic_model.py
@@ -7,16 +7,18 @@ import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 from parameters import *
 
+
 def solve_Vsa(Vsa0, Csa, Csa_l, Rs_u, Rs_l, Csa_u, Pext_l, Psa_u, Psv_l, rho,
               g, H, Q):
-    dydt = Q - Vsa/Csa*Rs_u - Vsa/Csa*Rs_l - (((-Vsa0 + Csa_l * Pext_l - Csa_l*rho*g*H)/Csa
-                                ) - Psv_u) / Rs_u  - ( 
-                                ((-Vsa0 + Csa_l * Pext_l - Csa_u*rho*g*H)/Csa
-                                ) - Psv_l) / Rs_l
-    
+
+    dydt = Q - Vsa/Csa(1/Rs_u + 1/Rs_l) - (
+            ((-Vsa0 + Csa_l * Pext_l - Csa_l*rho*g*H)/Csa) - Psv_u) / Rs_u - (
+            ((-Vsa0 + Csa_l * Pext_l - Csa_u*rho*g*H)/Csa
+            ) - Psv_l) / Rs_l
+    return dydt
 
 
-T = np.linspace(0,100,1)
+T = np.linspace(0, 100, 1)
 n_t = len(T)
 
 # TODO : change to vary gravity as fn of time
@@ -45,7 +47,6 @@ Hl = Hl_patient
 H = Hu + Hl
 
 
-
 Q = C_RVD * F*(P_ra - P_thorax)
 
 # assume venous vol is roughly 70% of total BV.
@@ -54,8 +55,10 @@ pct_venous = 0.7
 pct_arterial = 1 - pct_venous
 init_Vsa = Vtotal * pct_arterial
 init_Vsv = Vtotal * pct_venous
-Vsa = np.zeros(T) ; Vsa[0] = init_Vsa
-Vsv = np.zeros(T) ; Vsv[0] = init_Vsv
+Vsa = np.zeros(T)
+Vsa[0] = init_Vsa
+Vsv = np.zeros(T)
+Vsv[0] = init_Vsv
 
 # reseve volumes
 Vsa0 = np.zeros(T)
@@ -67,41 +70,40 @@ for t in T:
     if P_thorax[j] <= - dP_RA:
         # eq 20 => six cases for Pra:
         # eq 20 gives Pra
-        if Pext_l[t] < rho* g[t] * Hu:
+        if Pext_l[t] < rho * g[t] * Hu:
             if P_ra[t] <= Pext_l[t]:
                 P_ra[t] = (Vsv[t] - Vsv0[t] -
-                            Csv_l*(rho*g[t]*Hl) +
-                            Cra * P_thorax[t]) / Cra
+                           Csv_l*(rho*g[t]*Hl) +
+                           Cra * P_thorax[t]) / Cra
             elif P_ra[t] >= Pext_l[t]:
-                P_ra[t] = (Vsv[t] - Vsv0[t] - Csv_l * 
-                            (rho * g[t] * Hl - Pext_l[t]) + Cra * P_thorax[t]
-                            ) / (Cra + Csv_l)
+                P_ra[t] = (Vsv[t] - Vsv0[t] - Csv_l *
+                           (rho * g[t] * Hl - Pext_l[t]) + Cra * P_thorax[t]
+                           ) / (Cra + Csv_l)
             elif rho * g[t] * Hu <= P_ra:
-                P_ra[t] = (Vsv[t] - Vsv0[t] - Csv_l  * (rho * g * Hl 
-                            - Pext_l[t]) + Csv_u * rho * g *Hu + Cra * 
-                            P_thorax(t)) / (Cra + Csv_u + Csv_l)
+                P_ra[t] = (Vsv[t] - Vsv0[t] - Csv_l * (rho * g * Hl
+                                                       - Pext_l[t]) + Csv_u * rho * g * Hu + Cra *
+                           P_thorax(t)) / (Cra + Csv_u + Csv_l)
         else:
             if P_ra[t] <= rho * g[t] * Hu:
-                P_ra[t] = (Vsv[t] - Vsv0[t] - Csv_l * (rho * g[t] * Hl 
-                            - Pext_l[t]) + Cra * P_thorax[t] 
-                            - Csv_l*Pext_l[t]) / Cra
+                P_ra[t] = (Vsv[t] - Vsv0[t] - Csv_l * (rho * g[t] * Hl
+                                                       - Pext_l[t]) + Cra * P_thorax[t]
+                           - Csv_l*Pext_l[t]) / Cra
             elif P_ra[t] >= rho * g[t] * Hu:
-                P_ra[t] = (Vsv[t] - Vsv0[t] - Csv_l * rho * g[t] * Hl + 
+                P_ra[t] = (Vsv[t] - Vsv0[t] - Csv_l * rho * g[t] * Hl +
                            Csv_u * rho * g[t] * Hu + Cra * P_thorax[t]
-                          ) / (Cra + Csv_u)
+                           ) / (Cra + Csv_u)
             elif Pext_l[t] <= P_ra[t]:
                 P_ra[t] = (Vsv[t] - Vsv0[t] - Csv_l * (rho * g[t] * Hl -
                            Pext_l[t]) + Csv_u * rho * g[t] * Hu + Cra * P_thorax
-                          ) / (Cra + Csv_u + Csv_l)
-    
- 
-        Psv_u[t] = max(0, P_ra[t] - rho * g[t] * Hu) # eq 15
-        Psv_l[t] = max(P_ra,Pext_l) +rho*g[t]*Hl # eq 18
+                           ) / (Cra + Csv_u + Csv_l)
 
-        Psa_u[t] = (Vsa[t] - Vsa0[0] + Csa_l*Pext_l[t] - Csa_l* rho * g[t]*H)/(
-                    Csa) # eq 12
-        Psa_l[t] = (Vsa[t] - Vsa0[0] + Csa_l*Pext_l[t] - Csa_u* rho * g[t]*H)/(
-                    Csa) # eq 13
+        Psv_u[t] = max(0, P_ra[t] - rho * g[t] * Hu)  # eq 15
+        Psv_l[t] = max(P_ra, Pext_l) + rho*g[t]*Hl  # eq 18
+
+        Psa_u[t] = (Vsa[t] - Vsa0[0] + Csa_l*Pext_l[t] - Csa_l * rho * g[t]*H)/(
+            Csa)  # eq 12
+        Psa_l[t] = (Vsa[t] - Vsa0[0] + Csa_l*Pext_l[t] - Csa_u * rho * g[t]*H)/(
+            Csa)  # eq 13
 
     # Case II
     elif P_thorax[t] > - dP_RA and P_thorax[t] < rho * g[t] * Hu - dP_RA:
@@ -148,7 +150,7 @@ x0 = 6*[Qrv]
 test = volume_odes(x0, 0)
 
 # timestep
-t = np.linspace(0,3*60)
+t = np.linspace(0, 3*60)
 
 # SOLVE
 x = odeint(volume_odes, x0, t)
