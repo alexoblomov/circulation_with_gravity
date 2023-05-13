c311cadd46b2236cdb78d2a806e46940610c7747
alanna kennard <alanna.kennard@smartmeup.io>
2023-05-13 15:39:17 +0200

    update plot g profile script

--- dynamic_model/plot_g_profiles.py
+++ dynamic_model/plot_g_profiles.py
@@ -2,29 +2,14 @@ import csv
 from pathlib import Path
 import pandas as pd
 import matplotlib.pyplot as plt
+from utils.io import import_g_profile
 
 path = Path("dynamic_model/g_profiles")
-# fname = path / "1_Gz.csv"
-fname = path / "1_Gz.csv"
-# with open(fname, newline='') as csvfile:
-    # data = list(csv.reader(csvfile))
+fname = path / "Run_1_G.csv"
 
-with open(fname, 'r') as f:
-    data = f.readlines()
-# df= pd.read_csv(fname)
+T, g_range = import_g_profile(fname)
 
-# df_temp = df.iloc[:,0:2]
-# data = df.values.tolist()
-# oX = [line.replace('\n', '').split(',')[0] for line in data]
-# oY = [float(line.replace('\n', '').split(',')[1]) for line in data]
-# print(data)
-
-df = pd.read_csv(fname, usecols=[0,1], header=0)
-t= df['t'].to_numpy()
-gz = df['Gz'].to_numpy()
 
 plt.figure()
-plt.plot(t, gz)
+plt.plot(T, g_range)
 plt.savefig("g_1_profile.png")
-print(df)
-breakpoint()
